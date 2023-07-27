d0e4f87be88c5ee706a8946fd7a3f412d0db879b
Anchit Sinha <anchit1909@gmail.com>
2023-07-27 15:34:10 +0530

    Added Data

---
+++ src/utils/data.js
@@ -0,0 +1,65 @@
+const data = [
+  {
+    id: 1,
+    region: "US",
+    data: 290,
+    coordinate: [41, 20],
+  },
+  {
+    id: 2,
+    region: "US",
+    data: 490,
+    coordinate: [41.938936, -109.651463],
+  },
+
+  {
+    id: 3,
+    region: "AT",
+    data: 790,
+    coordinate: [-39.19408, -67.815526],
+  },
+  {
+    id: 4,
+    region: "SE",
+    data: 590,
+    coordinate: [9.8, 50.0001],
+  },
+  {
+    id: 5,
+    region: "AT",
+    data: 290,
+    coordinate: [-26.19408, -60.815526],
+  },
+  {
+    id: 6,
+    region: "SE",
+    data: 390,
+    coordinate: [6.864266, 20.196193],
+  },
+  {
+    id: 7,
+    region: "US",
+    data: 490,
+    coordinate: [37.839333, -84.27002],
+  },
+  {
+    id: 8,
+    region: "US",
+    data: 690,
+    coordinate: [16.490067, -90.475681],
+  },
+  {
+    id: 9,
+    region: "Asia",
+    data: 600,
+    coordinate: [20.5937, 78.9629],
+  },
+  {
+    id: 10,
+    region: "Asia",
+    data: 200,
+    coordinate: [63.372464, 97.473537],
+  },
+];
+
+export default data;
